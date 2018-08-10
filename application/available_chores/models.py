44a59b26c419bd82f2f699a3eceae03495b322b1
Jussi Steenari <jussi.steenari@helsinki.fi>
2018-08-10 19:41:54 +0300

    korjattu tietokantaa

--- application/available_chores/models.py
+++ application/available_chores/models.py
@@ -8,7 +8,7 @@ class AvailableChore(db.Model):
     householdid = db.Column(db.Integer, db.ForeignKey('household.id'), nullable=False)
     points = db.Column(db.Integer, nullable=False)
     maxpoints = db.Column(db.Integer, nullable=False)
-    choretype= db.Column(db.Integer, nullable= False)
+    choretype= db.Column(db.String(144), nullable= False)
     message= db.Column(db.String, nullable= True)
     
     def __init__(self, householdid, points, choretype):
