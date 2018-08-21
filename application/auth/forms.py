3c77855e10bf81fc631cdb91f1e64712c05d83c8
Jussi Steenari <jussi.steenari@helsinki.fi>
2018-08-21 22:46:25 +0300

    heroku

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -6,10 +6,10 @@ from wtforms import PasswordField, StringField, SelectField, validators
 #from application.households.models import Household
 
 class UserForm(FlaskForm):
-    households=[]
-    h=Household.query.all()
-    for house in h:
-        households.append((house.id, house.name))
+    #households=[]
+    #h=Household.query.all()
+    #for house in h:
+        #households.append((house.id, house.name))
     name= StringField("Nimi",[validators.Length(min=2, max=32, message="Nimi pitää olla ainakin 2 ja korkeintaan 32 merkkiä pitkä")])
     username = StringField("Käyttäjätunnus",[validators.Length(min=2, max=32, message="Käyttäjätunnus pitää olla ainakin 2 ja korkeintaan 32 merkkiä pitkä")])
     password = PasswordField("Salasana", [validators.Length(min=2, max=32, message="Salasanan pitää olla ainakin 2 ja korkeintaan 32 merkkiä pitkä")])
@@ -17,7 +17,7 @@ class UserForm(FlaskForm):
     
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
-        self.household.choices = [(h.id, h.name) for h in Household.query.all()]
+        #self.household.choices = [(h.id, h.name) for h in Household.query.all()]
     
     class Meta:
         csrf = False
